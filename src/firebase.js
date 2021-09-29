700e596725528feb8c7976cd3d9db594caaf41cb
Gozel Cholukova <checkboxtm@gmail.com>
2021-09-30 00:04:03 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,8 @@
 import firebase from "firebase";
 
 var firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyAFqs-2CQB-VtvV1DmQyA6amOvHSLgZIEA",
-  authDomain: "yyldyz-hotel-demo.firebaseapp.com",
-  databaseURL: "https://yyldyz-hotel-demo-default-rtdb.firebaseio.com",
-  projectId: "yyldyz-hotel-demo",
-  storageBucket: "yyldyz-hotel-demo.appspot.com",
-  messagingSenderId: "494551316093",
-  appId: "1:494551316093:web:7e6f28f0e1e88cb69aa314"
+  // Your firebase credentials
 })
-
 
 var db = firebaseApp.firestore();
 
