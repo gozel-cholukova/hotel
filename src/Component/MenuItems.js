35575056601c75c778c5ae0a05ea553d5437103c
Gozel Cholukova <gozelr@mail.ru>
2021-11-12 20:56:54 +0500

    Add files via upload

---
+++ src/Component/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    title: 'SPA',
+    path: '/marketing',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Restaurant',
+    path: '/consulting',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Bar',
+    path: '/design',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Conference Hall',
+    path: '/development',
+    cName: 'dropdown-link'
+  }
+];
